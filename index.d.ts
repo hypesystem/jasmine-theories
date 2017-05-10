0a08317e3f62d28372409937b890985b2c3f078f
Bart Ledoux <ledouxb@gmail.com>
2017-05-10 08:40:55 -0500

    Add declaration file for theories

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare namespace theoretically {
+    export function it(description:string, args:any[], testFunction:Function):void;
+    export function xit(description:string, args:any[], testFunction:Function):void;
+}
+export default theoretically
