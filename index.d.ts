dcf5e158211477e1750c6135dc903fb4bead3fa8
Bart Ledoux <ledouxb@gmail.com>
2017-05-14 14:06:43 -0500

    document tests better

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,21 @@
-declare namespace theoretically {
-    export function it(description:string, args:any[], testFunction:Function):void;
-    export function xit(description:string, args:any[], testFunction:Function):void;
-}
-export default theoretically
+declare namespace jasmineTheories {
+	interface jasmineTheoriesStatic {
+		/**
+		 * Runs the jasmine test function with each of the given arguments in the list
+		 * @param description the description of the test
+		 * @param args argunments passed to the test
+		 * @param testFunction the test
+		 */
+		it(description: string, args: any[], testFunction: (testedValue: any) => void): void;
+ 
+		/**
+		 * Ignored version of the "it" test (the x means excluded)
+		 * @param description the description of the test
+		 * @param args argunments passed to the test
+		 * @param testFunction the test
+		 */
+		xit(description: string, args: any[], testFunction: (testedValue: any) => void): void;
+	}
+ }
+ declare var jsminTheories: jasmineTheories.jasmineTheoriesStatic;
+ export default jsminTheories;
