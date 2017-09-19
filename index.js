0ffc7b767bcc4e2fbae6f04c48961a5e775872fc
Bart Ledoux <bart.ledoux@enablon.com>
2017-09-19 14:05:26 -0500

    Fix Typescript default exports
    according to http://2ality.com/2014/09/es6-modules-final.html#default-exports-one-per-module the correct way to export would be to export an item called default.
    WIthoiut this extra item it is working fine in karma. In wallabyjs it is failing miserably, ignoring every theory test. With this change it works again.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = {
     it: require("./lib/theoretically.it.js"),
-    xit: require("./lib/theoretically.xit.js")
+    xit: require("./lib/theoretically.xit.js"),
+    default: {
+        it: require("./lib/theoretically.it.js"),
+        xit: require("./lib/theoretically.xit.js")
+    }
 };
