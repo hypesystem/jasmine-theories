cccf32dafd5203935e6f2523c6afa7d3265e3e63
Azat Galiev <azat.galiev@arcadia.spb.ru>
2020-03-08 23:54:23 +0300

    Added IDE code completion support for the done.fail() invocation.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,16 @@
 declare namespace jasmineTheories {
+	/**
+	 * Action method that should be called when the async work is complete.
+	 */
+	interface DoneFn extends Function {
+		(): void;
+
+		/** fails the spec and indicates that it has completed. If the message is an Error, Error.message is used */
+		fail: (message?: Error | string) => void;
+	}
+
+	type ImplementationCallback = ((testedValue: any) => PromiseLike<any>) | ((testedValue: any, done: DoneFn) => void);
+
 	interface jasmineTheoriesStatic {
 		/**
 		 * Runs the jasmine test function with each of the given arguments
@@ -6,15 +18,15 @@ declare namespace jasmineTheories {
 		 * @param args argunments passed to the test
 		 * @param testFunction the test
 		 */
-		it(description: string, args: any[], testFunction: (testedValue: any, done?: () => void) => void): void;
- 
+		it(description: string, args: any[], testFunction: ImplementationCallback): void;
+
 		/**
 		 * Ignored version of the "it" test (the x means excluded)
 		 * @param description the description of the test
 		 * @param args argunments passed to the test
 		 * @param testFunction the test
 		 */
-		xit(description: string, args: any[], testFunction: (testedValue: any, done?: () => void) => void): void;
+		xit(description: string, args: any[], testFunction: ImplementationCallback): void;
 	}
  }
  declare var jsminTheories: jasmineTheories.jasmineTheoriesStatic;
