26032fe40f6d1dff7b88da4698f21e8de50771dd
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-05-19 14:03:56 +0200

    Fix indentation

--- spec/theoretically.it.spec.js
+++ spec/theoretically.it.spec.js
@@ -5,131 +5,131 @@ var theoretically = require("../index.js");
     { key: "it", name: "A theory" },
     { key: "xit", name: "A pending theory" },
 ].forEach(({ key, name }) => {
-describe(name, function() {
-    var oldIt;
+    describe(name, function() {
+        var oldIt;
 
-    beforeEach(function() {
-        oldIt = global[key];
-    });
-
-    afterEach(function() {
-        global[key] = oldIt;
-    });
-
-    it("runs `it` with each value in the value array and a formatted message", function() {
-        it = jasmine.createSpy("it");
-
-        var handler = jasmine.createSpy("handler");
-        theoretically.it("might run a test with %s as the value", [ "x", "y", "z" ], handler);
-
-        expect(it).toHaveBeenCalledWith("might run a test with x as the value", jasmine.any(Function));
-        expect(it).toHaveBeenCalledWith("might run a test with y as the value", jasmine.any(Function));
-        expect(it).toHaveBeenCalledWith("might run a test with z as the value", jasmine.any(Function));
-
-        var itCalledWithX = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "might run a test with x as the value";
+        beforeEach(function() {
+            oldIt = global[key];
         });
 
-        var itCalledWithY = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "might run a test with y as the value";
+        afterEach(function() {
+            global[key] = oldIt;
         });
 
-        var itCalledWithZ = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "might run a test with z as the value";
+        it("runs `it` with each value in the value array and a formatted message", function() {
+            it = jasmine.createSpy("it");
+
+            var handler = jasmine.createSpy("handler");
+            theoretically.it("might run a test with %s as the value", [ "x", "y", "z" ], handler);
+
+            expect(it).toHaveBeenCalledWith("might run a test with x as the value", jasmine.any(Function));
+            expect(it).toHaveBeenCalledWith("might run a test with y as the value", jasmine.any(Function));
+            expect(it).toHaveBeenCalledWith("might run a test with z as the value", jasmine.any(Function));
+
+            var itCalledWithX = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "might run a test with x as the value";
+            });
+
+            var itCalledWithY = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "might run a test with y as the value";
+            });
+
+            var itCalledWithZ = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "might run a test with z as the value";
+            });
+
+            if(!itCalledWithX || !itCalledWithY || !itCalledWithZ) {
+                return expect("Failed to get one of the calls").toBeFalsy();
+            }
+
+            expect(handler).not.toHaveBeenCalled();
+
+            var xHandler = itCalledWithX[1];
+            xHandler();
+
+            expect(handler.calls.count()).toEqual(1);
+            expect(handler).toHaveBeenCalledWith("x");
+
+            var yHandler = itCalledWithY[1];
+            var fakeCallback = { fakeCallback: true };
+            yHandler(fakeCallback);
+
+            expect(handler.calls.count()).toEqual(2);
+            expect(handler).toHaveBeenCalledWith("y", fakeCallback);
+
+            var zHandler = itCalledWithZ[1];
+            zHandler();
+
+            expect(handler.calls.count()).toEqual(3);
+            expect(handler).toHaveBeenCalledWith("z");
         });
 
-        if(!itCalledWithX || !itCalledWithY || !itCalledWithZ) {
-            return expect("Failed to get one of the calls").toBeFalsy();
-        }
+        it("runs `it` with each value in the array and a message with the argument appended", function() {
+            it = jasmine.createSpy("it");
 
-        expect(handler).not.toHaveBeenCalled();
+            var handler = jasmine.createSpy("handler");
+            theoretically.it("can handle this value:", [ 1, 2, 3 ], handler);
 
-        var xHandler = itCalledWithX[1];
-        xHandler();
+            expect(it).toHaveBeenCalledWith("can handle this value: 1", jasmine.any(Function));
+            expect(it).toHaveBeenCalledWith("can handle this value: 2", jasmine.any(Function));
+            expect(it).toHaveBeenCalledWith("can handle this value: 3", jasmine.any(Function));
 
-        expect(handler.calls.count()).toEqual(1);
-        expect(handler).toHaveBeenCalledWith("x");
+            var itCalledWithX = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "can handle this value: 1";
+            });
 
-        var yHandler = itCalledWithY[1];
-        var fakeCallback = { fakeCallback: true };
-        yHandler(fakeCallback);
+            var itCalledWithY = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "can handle this value: 2";
+            });
 
-        expect(handler.calls.count()).toEqual(2);
-        expect(handler).toHaveBeenCalledWith("y", fakeCallback);
+            var itCalledWithZ = _.find(it.calls.allArgs(), function(args) {
+                return args[0] == "can handle this value: 3";
+            });
 
-        var zHandler = itCalledWithZ[1];
-        zHandler();
+            if(!itCalledWithX || !itCalledWithY || !itCalledWithZ) {
+                return expect("Failed to get one of the calls: " + itCalledWithX + " " + itCalledWithY + " " + itCalledWithZ).toBeFalsy();
+            }
 
-        expect(handler.calls.count()).toEqual(3);
-        expect(handler).toHaveBeenCalledWith("z");
-    });
+            expect(handler).not.toHaveBeenCalled();
 
-    it("runs `it` with each value in the array and a message with the argument appended", function() {
-        it = jasmine.createSpy("it");
+            var xHandler = itCalledWithX[1];
+            xHandler();
 
-        var handler = jasmine.createSpy("handler");
-        theoretically.it("can handle this value:", [ 1, 2, 3 ], handler);
+            expect(handler.calls.count()).toEqual(1);
+            expect(handler).toHaveBeenCalledWith(1);
 
-        expect(it).toHaveBeenCalledWith("can handle this value: 1", jasmine.any(Function));
-        expect(it).toHaveBeenCalledWith("can handle this value: 2", jasmine.any(Function));
-        expect(it).toHaveBeenCalledWith("can handle this value: 3", jasmine.any(Function));
+            var yHandler = itCalledWithY[1];
+            var fakeCallback = { fakeCallback: true };
+            yHandler(fakeCallback);
 
-        var itCalledWithX = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "can handle this value: 1";
+            expect(handler.calls.count()).toEqual(2);
+            expect(handler).toHaveBeenCalledWith(2, fakeCallback);
+
+            var zHandler = itCalledWithZ[1];
+            zHandler();
+
+            expect(handler.calls.count()).toEqual(3);
+            expect(handler).toHaveBeenCalledWith(3);
         });
 
-        var itCalledWithY = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "can handle this value: 2";
+        it("throws if `it` isn't available in global scope", function() {
+            delete it;
+
+            expect(function() {
+                theoretically.it("can handle this value:", [1, 2, 3], function() {});
+            }).toThrow(jasmine.any(ReferenceError));
         });
 
-        var itCalledWithZ = _.find(it.calls.allArgs(), function(args) {
-            return args[0] == "can handle this value: 3";
+        it("fails if args is not an array", function() {
+            expect(function() {
+                theoretically.it("can handle this value:", "not an array", function() {});
+            }).toThrow(jasmine.any(Error));
         });
 
-        if(!itCalledWithX || !itCalledWithY || !itCalledWithZ) {
-            return expect("Failed to get one of the calls: " + itCalledWithX + " " + itCalledWithY + " " + itCalledWithZ).toBeFalsy();
-        }
-
-        expect(handler).not.toHaveBeenCalled();
-
-        var xHandler = itCalledWithX[1];
-        xHandler();
-
-        expect(handler.calls.count()).toEqual(1);
-        expect(handler).toHaveBeenCalledWith(1);
-
-        var yHandler = itCalledWithY[1];
-        var fakeCallback = { fakeCallback: true };
-        yHandler(fakeCallback);
-
-        expect(handler.calls.count()).toEqual(2);
-        expect(handler).toHaveBeenCalledWith(2, fakeCallback);
-
-        var zHandler = itCalledWithZ[1];
-        zHandler();
-
-        expect(handler.calls.count()).toEqual(3);
-        expect(handler).toHaveBeenCalledWith(3);
+        it("fails if args is an empty array", function() {
+            expect(function() {
+                theoretically.it("can handle this value:", [], function() {});
+            }).toThrow(jasmine.any(Error));
+        });
     });
-
-    it("throws if `it` isn't available in global scope", function() {
-        delete it;
-
-        expect(function() {
-            theoretically.it("can handle this value:", [1, 2, 3], function() {});
-        }).toThrow(jasmine.any(ReferenceError));
-    });
-
-    it("fails if args is not an array", function() {
-        expect(function() {
-            theoretically.it("can handle this value:", "not an array", function() {});
-        }).toThrow(jasmine.any(Error));
-    });
-
-    it("fails if args is an empty array", function() {
-        expect(function() {
-            theoretically.it("can handle this value:", [], function() {});
-        }).toThrow(jasmine.any(Error));
-    });
-});
 });
